18c54bb94226dbaa8063f05d58c46198539cfbf5
dinin538 <dnnblh@gmail.com>
2021-09-30 10:47:32 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <?php
+    $nama = 'NOR DINI NABILAH BT HALIM';
+    $umur = 20;
+    
+    echo "$nama berumur $umur tahun";
+    ?>
+    
+</body>
+</html>
